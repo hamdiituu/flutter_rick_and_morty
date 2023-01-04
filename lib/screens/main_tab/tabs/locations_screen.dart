9d28c8683412f41fcb2cb354c0fa657ed8355755
Hamdi Tuğ <62481358+hamdiituu@users.noreply.github.com>
2023-01-04 15:55:49 +0300

    application base add

---
+++ lib/screens/main_tab/tabs/locations_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LocationsScreen extends StatelessWidget {
+  const LocationsScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Locations"),
+    );
+  }
+}
